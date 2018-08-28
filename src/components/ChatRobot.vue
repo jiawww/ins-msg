e0a1be12fff7a03dc7128b2c78083fa05d988bad
zj <zhangjia@agree.com.cn>
2018-08-28 23:48:39 +0800

    加动画

---
+++ src/components/ChatRobot.vue
@@ -0,0 +1,63 @@
+<!--  -->
+<template>
+  <div class="chat-robot">
+    <div class="robot aikf-cartoon-red-showBtn" :style="{backgroundPosition:backgroundPosition}" @mouseover="over" @mouseout="clear"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'chat-robot',
+  data () {
+    return {
+      backgroundPosition:"0px",
+      timer:null
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted(){},
+
+  methods: {
+    over(){ 
+     let cartoonIndex = 0;
+     let _this=this;
+     this.timer = setInterval(function () {
+        cartoonIndex++;
+        if (cartoonIndex >= 18) {
+          cartoonIndex = 0;
+        }
+          _this.backgroundPosition = -cartoonIndex * 100 + 'px';
+        }, 80)
+    },
+    clear(){
+      clearInterval(this.timer);
+      this.timer=null;
+
+    }
+  }
+}
+
+</script>
+<style lang='less' scoped>
+  .chat-robot{
+    .robot{
+      position: absolute;
+      left:0px;
+      bottom:0px;
+      color:#fff;
+      font-size:16px;
+      text-align: center;
+      cursor:pointer;
+    }
+    .aikf-cartoon-red-showBtn{
+      width:100px;
+      height:270px;
+      background: url(../../public/image/cartoon-red.png) left top no-repeat;
+    }
+
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
+    <h1>对话AI</h1>
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <chat-robot></chat-robot>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ChatRobot from '@/components/ChatRobot.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    ChatRobot
   }
 }
 </script>
